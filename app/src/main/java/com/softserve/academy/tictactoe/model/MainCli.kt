b424411fa06473fd1ad86062a3d647abb8f2cae0
obutr <butrym@karazin.ua>
2024-12-23 20:48:09 +0200

    refactor model and add a test for click

--- app/src/main/java/com/softserve/academy/tictactoe/model/MainCli.kt
+++ app/src/main/java/com/softserve/academy/tictactoe/model/MainCli.kt
@@ -2,15 +2,26 @@ package com.softserve.academy.tictactoe.model
 
 import com.softserve.academy.tictactoe.model.GameState.*
 
-typealias Field = CharArray
+typealias Field = List<CellState>
 
-fun String.toField(): Field = this.toCharArray()
-
-var data: Field = "0X0X0X0X0".toField()
+fun String.toField(): Field {
+    val res = mutableListOf<CellState>()
+    for (ch in this) {
+       when(ch) {
+           'x', 'X' -> res.add(CellState.CROSS)
+           '0', 'o', 'O' -> res.add(CellState.NOUGHT)
+           '_', ' ' -> res.add(CellState.EMPTY)
+       }
+    }
+    return res
+}
 
 const val DIM = 3
 
 operator fun Field.get(row: Int, col: Int) = this[ix(row, col)]
+//operator fun Field.set(row: Int, col: Int, cell: CellState) {
+//    this[ix(row, col)] = cell
+//}
 
 fun ix(row: Int, col: Int) = row * DIM + col
 
@@ -23,6 +34,17 @@ fun printField(field: Field) {
     }
 }
 
+enum class CellState {
+    EMPTY, CROSS, NOUGHT;
+
+    override fun toString(): String = when(this) {
+        EMPTY -> "_"
+        CROSS -> "X"
+        NOUGHT -> "0"
+    }
+}
+
+
 enum class GameState {
     IN_PROGRESS, CROSS_WIN, NOUGHT_WIN, DRAW, ILLEGAL
 }
@@ -31,9 +53,13 @@ enum class Player {
     CROSS, NOUGHT
 }
 
-fun Player.toChar(): Char = when(this) {
-    Player.CROSS -> 'X'
-    Player.NOUGHT -> '0'
+//fun Player.toChar(): Char = when(this) {
+//    Player.CROSS -> 'X'
+//    Player.NOUGHT -> '0'
+//}
+val Player.mark get() = when(this) {
+    Player.CROSS -> CellState.CROSS
+    Player.NOUGHT -> CellState.NOUGHT
 }
 
 //fun gameState(field: Field): GameState
@@ -51,43 +77,43 @@ val Field.gameState: GameState get() {
         arrayOf(0, 4, 8),
         arrayOf(2, 4, 6),
     )
-    val nX = this.count { it == 'X' }
-    val n0 = this.count { it == '0' }
+    val nX = this.count { it == CellState.CROSS }
+    val n0 = this.count { it == CellState.NOUGHT }
     if ((nX == n0 || nX == n0 + 1).not()) {
         return ILLEGAL
     }
     if (nX + n0 == DIM * DIM) return DRAW
     for (line in lines) {
         when {
-            line.all { this[it] == 'X' } -> return CROSS_WIN
-            line.all { this[it] == '0' } -> return NOUGHT_WIN
+            line.all { this[it] == CellState.CROSS }  -> return CROSS_WIN
+            line.all { this[it] == CellState.NOUGHT } -> return NOUGHT_WIN
         }
     }
     return IN_PROGRESS
 }
 
 val Field.nextPlayer: Player get() {
-    val nX = this.count { it == 'X' }
-    val n0 = this.count { it == '0' }
+    val nX = this.count { it == CellState.CROSS }
+    val n0 = this.count { it == CellState.NOUGHT }
     return if (nX > n0) Player.NOUGHT else Player.CROSS
 }
 
 fun Field.click(row: Int, col: Int): Field {
-    if (this[ix(row, col)] != '_') return this
-    val res = clone()
-    res[ix(row, col)] = nextPlayer.toChar()
+    if (this[ix(row, col)] != CellState.EMPTY) return this
+    val res = this.toMutableList()
+    res[ix(row, col)] = nextPlayer.mark
     return res
 }
 
 fun main() {
+    val data: Field = "0X0|X0X|0X0".toField()
     printField(data)
 //    println(gameState(data))
     println(data.gameState)
-//    println("XXXXXXXXX".toField().gameState)
+    println("XXX|XXX|XXX".toField().gameState)
     println("XXX_0_00_".toField().gameState)
     println("000_X_XX_".toField().gameState)
     println("X________".toField().nextPlayer) // > NOUGHT
     println("X0_______".toField().nextPlayer) // > CROSS
     println("X0_______".toField().click(0, 2).toList() == "X0X______".toField().toList())
-    printField("_________".toCharArray())
 }
--- app/src/test/java/com/softserve/academy/tictactoe/CliTest.kt
+++ app/src/test/java/com/softserve/academy/tictactoe/CliTest.kt
@@ -2,6 +2,7 @@ package com.softserve.academy.tictactoe
 
 import com.softserve.academy.tictactoe.model.GameState
 import com.softserve.academy.tictactoe.model.Player
+import com.softserve.academy.tictactoe.model.click
 import com.softserve.academy.tictactoe.model.gameState
 import com.softserve.academy.tictactoe.model.nextPlayer
 import com.softserve.academy.tictactoe.model.toField
@@ -22,11 +23,30 @@ class CliTest {
 
     @Test
     fun `when there is a single cross on the field then next player should be nought`() {
-//        println("X________".toField().nextPlayer)
         val field = "X________".toField()
 
         val res = field.nextPlayer
 
         assertEquals(Player.NOUGHT, res)
+    }
+
+    @Test
+    fun testClick1() {
+        val initial = "X0_______".toField()
+        val expected = "X0X______".toField()
+
+        val afterClick = initial.click(0, 2)
+
+        assertEquals(expected.toList(), afterClick.toList())
+    }
+
+    @Test
+    fun testClick2() {
+        val initial = "X0X______".toField()
+        val expected = initial
+
+        val afterClick = initial.click(0, 2)
+
+        assertEquals(expected.toList(), afterClick.toList())
     }
 }
